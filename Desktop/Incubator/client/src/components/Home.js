ef8c9dfebea3fa67082654202b823180a8f506c6
Jakub Czachor <42331274+JakubCzachor@users.noreply.github.com>
2023-02-02 20:48:55 -0500

    Update Home.js

--- Desktop/Incubator/client/src/components/Home.js
+++ Desktop/Incubator/client/src/components/Home.js
@@ -1,15 +1,42 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState} from 'react'
 import { useNavigate } from 'react-router-dom'
+import styled from 'styled-components';
 import "../App.css"
+import "../fire.js"
+import { app } from '../fire';
+import { getFirestore } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../fire";
+import Header from "../header";
+import AuthHeader from "../AuthHeader";
+import Button from '@mui/material/Button';
+
+const AnimatedButton = styled.button`
+  background-color: #4CAF50;
+  color: white;
+  transition: all 0.5s;
+
+  &:hover {
+    transform: scale(1.2);
+  }
+`;
+
+
 export default function Home() {
+    const [tasks, setTasks] = useState([]);
+
     const handleLogout = () => {
         sessionStorage.removeItem('Auth Token');
+        sessionStorage.removeItem('Email');
         navigate('/login')
     }
+
     let navigate = useNavigate();
     useEffect(() => {
         let authToken = sessionStorage.getItem('Auth Token')
+        let emailToken = sessionStorage.getItem('Email')
         console.log(authToken)
+        console.log(emailToken)
         if (authToken) {
             navigate('/home')
         }
@@ -17,12 +44,42 @@ export default function Home() {
         if (!authToken) {
             navigate('/login')
         }
-    }, [])
-    return (
-        <div className= "homepage">
-            Home Page
 
-            <button onClick={handleLogout}>Log out</button>
-        </div>
-    )
+        const fetchData = async () => {
+            const querySnapshot = await getDocs(collection(db, "Tasks"));
+            const taskArray = [];
+            querySnapshot.forEach((doc) => {
+                taskArray.push({
+                    name: doc.data().name,
+                    assignedUser: doc.data().assignedUser,
+                    description: doc.data().description,
+                    email: doc.data().emails,
+                });
+            });
+            setTasks(taskArray);
+        };
+        fetchData();
+
+    }, []);
+
+
+
+
+        return (
+            <div className="homepage">
+                <h1>All Available Tasks: </h1>
+                <ul>
+                    {tasks.map((task) => (
+                        <li key={task.id}>
+                            <p>Name: {task.name}</p>
+                            <p>Assigned User: {task.assignedUser}</p>
+                            <p>Description: {task.description}</p>
+                            <p>Email: {task.email } </p>
+                        </li>
+                    ))}
+                </ul>
+                <Button variant = "contained" onClick={handleLogout}>Log out</Button>
+
+            </div>
+        );
 }
