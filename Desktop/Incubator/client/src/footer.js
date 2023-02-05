5092443d86e701d3912d2b0a2a44d851b8c9a698
Jakub Czachor <42331274+JakubCzachor@users.noreply.github.com>
2023-02-05 17:48:55 -0500

    Create footer.js

---
+++ Desktop/Incubator/client/src/footer.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { React } from 'react';
+
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="cont">
+                <div className="left" ><p>Jakub Czachor, Mouhamed Dia, Vianey Tlapechco </p> </div>
+
+                <div className="right"><p> czachorjjakub@gmail.com, vianey.tlapechco@gmail.com,  </p></div>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
